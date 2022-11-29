ba9e025ac3cb43c41030c76e5ff1f83a6752cda9
maksim <MaksB1988@yandex.ru>
2022-11-29 03:37:25 +0300

    Добавил начальные значения

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+string[] array = new string[] { "12", "324", "325", ":-)", "Hello", "MIR", "1", "world", "23654", "yes", "56123" };
+string count = string.Empty;
+
+for (int i = 0; i < array.Length; i++)
+{
+
+    if (array[i].Length <= 3)
+    {
+        count = array[i];
+        Console.Write($"{count}, ");
+    }
+
+}
+
