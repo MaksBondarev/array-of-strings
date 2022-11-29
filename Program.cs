8eb29eef40d535b963524e5854c8aee4065c1595
maksim <MaksB1988@yandex.ru>
2022-11-29 03:40:57 +0300

    Добавили начальные значения массива

--- Program.cs
+++ Program.cs
@@ -3,14 +3,3 @@
 string[] array = new string[] { "12", "324", "325", ":-)", "Hello", "MIR", "1", "world", "23654", "yes", "56123" };
 string count = string.Empty;
 
-for (int i = 0; i < array.Length; i++)
-{
-
-    if (array[i].Length <= 3)
-    {
-        count = array[i];
-        Console.Write($"{count}, ");
-    }
-
-}
-
